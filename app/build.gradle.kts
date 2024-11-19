08a553253d4ac173d4703e1d5b02936cf02091fe
ErelWeinstein <erelweinstein123@gmail.com>
2024-11-19 10:15:03 +0200

    fixed login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation ("com.google.android.gms:play-services-auth:20.7.0")
+    implementation ("com.google.android.gms:play-services-auth:21.2.0")
 
 
 
